9e1e446441bc3e3a621aa75fb273bfe6236de235
Catherine David <miscatulated@gmail.com>
2021-06-17 20:00:54 -0700

    first

---
+++ main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+
+struct test {
+  char test[20];
+  int foo;
+  float bar;
+};
+
+int main() {
+  struct test a;
+  memcpy(a.test, "Hello", 5);
+  a.foo = 42;
+  a.bar = 0.1234;
+
+  FILE *f;
+  f = fopen("test.obj", "w");
+  fwrite(&a, sizeof(a), 1, f);
+  fclose(f);
+
+  // Read it back
+  struct test b;
+  f = fopen("test.obj", "r");
+  fread(&b, sizeof(b), 1, f);
+
+  printf("test %s, foo %d, bar %f\n", b.test, b.foo, b.bar);
+}
